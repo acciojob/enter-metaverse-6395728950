fe2c1489fce7229bb84394c22a81dc1d4c07ffef
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-28 23:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
  document.getElementById("enterBtn").addEventListener("click", function() {
-            const statusElement = document.getElementById("status");
-            statusElement.outerHTML = `<h1>Entered Metaverse</h1>`;
-        });
+    const statusElement = document.getElementById("status");
+    statusElement.innerHTML = `<h1>Entered Metaverse</h1>`;
+});
